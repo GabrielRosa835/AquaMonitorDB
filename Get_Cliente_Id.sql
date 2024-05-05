f6f81992b721db1eb97dfee9a3ef1b62e0e3e9fe
GabrielRosa835 <150252238+GabrielRosa835@users.noreply.github.com>
2024-05-05 10:36:47 -0300

    Finalizing
    Commented.

--- Get_Cliente_Id.sql
+++ Get_Cliente_Id.sql
@@ -1,11 +1,14 @@
 -- CREATE DATABASE db_upxII;
 USE db_upxII;
 
+/*	Function wich retrives the client's id from its registry contents.
+*/
+
 -- DROP FUNCTION get_cliente_id;
 CREATE FUNCTION get_cliente_id(
 	cliente_nome_in VARCHAR(100),
 	cliente_cpf_in VARCHAR(15),
-    cliente_telefone_in VARCHAR(20)
+    	cliente_telefone_in VARCHAR(20)
 ) RETURNS INT DETERMINISTIC
 RETURN (
 	SELECT Cliente_Id FROM tbl_clientes
