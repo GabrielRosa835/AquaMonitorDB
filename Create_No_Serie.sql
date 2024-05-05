f75ec8ce16d462d61f80b833f19505a4e72e552b
GabrielRosa835 <150252238+GabrielRosa835@users.noreply.github.com>
2024-05-05 10:34:17 -0300

    Finalizing
    Commented

--- Create_No_Serie.sql
+++ Create_No_Serie.sql
@@ -12,11 +12,12 @@ BEGIN
 	DECLARE new_no_serie INT DEFAULT -1;
 	SET new_no_serie = RAND() * 99999 + 1;
 	
-	WHILE 
+	WHILE -- Keeps creating a new random number until there's no match
 		(SELECT Dispositivo_NoSerie FROM tbl_dispositivos WHERE Dispositivo_NoSerie = new_no_serie) IS NOT NULL
 	DO
 		SET new_no_serie = RAND() * 99999 + 1;
 	END WHILE;
+
 	RETURN new_no_serie;
 END $
 DELIMITER ;
