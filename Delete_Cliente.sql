079c51f664158c2e0d6a3e8ea8b56951808f65d1
GabrielRosa835 <150252238+GabrielRosa835@users.noreply.github.com>
2024-05-05 10:31:47 -0300

    Finalizing
    Commented and corrected tabulation,

--- Delete_Cliente.sql
+++ Delete_Cliente.sql
@@ -1,6 +1,10 @@
 -- CREATE DATABASE db_upxII;
 USE db_upxII;
 
+/* 	Encapsulation for clients deletion.
+	It automatically deletes any relation conected to the client.
+*/
+
 -- DROP PROCEDURE delete_cliente;
 DELIMITER $
 CREATE PROCEDURE delete_cliente(
@@ -8,7 +12,7 @@ CREATE PROCEDURE delete_cliente(
 )
 BEGIN 
 	DELETE FROM tbl_clientes WHERE Cliente_Id = cliente_id_in;    
-    DELETE FROM tbl_cliente_endereco WHERE Cliente_Id = cliente_id_in;
-    DELETE FROM tbl_cliente_dispositivo WHERE Cliente_Id = cliente_id_in;
+    	DELETE FROM tbl_cliente_endereco WHERE Cliente_Id = cliente_id_in;
+    	DELETE FROM tbl_cliente_dispositivo WHERE Cliente_Id = cliente_id_in;
 END $
 DELIMITER ;
