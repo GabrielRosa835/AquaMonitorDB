0580bffd17b374ae55f0f1e107e2ad20e1e48ea5
GabrielRosa835 <150252238+GabrielRosa835@users.noreply.github.com>
2024-05-05 10:38:03 -0300

    Finalizing
    Commented and corrected tabulation.

--- Get_Endereco_Id.sql
+++ Get_Endereco_Id.sql
@@ -1,13 +1,16 @@
 -- CREATE DATABASE db_upxII;
 USE db_upxII;
 
+/*	Function wich retrives the adress' id from its registry contents.
+*/
+
 -- DROP FUNCTION get_endereco_id;
 CREATE FUNCTION get_endereco_id(
-    endereco_rua_in VARCHAR(50),
-    endereco_no_in INT,
-    endereco_bairro_in VARCHAR(50),
-    endereco_cep_in INT,
-    endereco_complemento_in VARCHAR(50)
+	endereco_rua_in VARCHAR(50),
+    	endereco_no_in INT,
+   	endereco_bairro_in VARCHAR(50),
+    	endereco_cep_in INT,
+    	endereco_complemento_in VARCHAR(50)
 ) RETURNS INT DETERMINISTIC
 RETURN (
 	SELECT Endereco_Id FROM tbl_enderecos
