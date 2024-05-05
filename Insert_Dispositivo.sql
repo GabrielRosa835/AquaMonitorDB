0f7d956495646bef9fd7c792a616defe107570af
GabrielRosa835 <150252238+GabrielRosa835@users.noreply.github.com>
2024-05-05 09:33:55 -0300

    Updated Insert_Dispositivo
    Removed the cliente_dispositivo relation, instead just keeping the endereco_dispositivo one.

--- Insert_Dispositivo.sql
+++ Insert_Dispositivo.sql
@@ -1,29 +1,25 @@
 -- CREATE DATABASE db_upxII;
 USE db_upxII;
 
--- DROP PROCEDURE insert_endereco;
+-- DROP PROCEDURE insert_dispositivo;
 DELIMITER $
 CREATE PROCEDURE insert_dispositivo(
-	cliente_id_in INT,
-    endereco_id_in INT,
-    dispositivo_noserie_in INT
+    	endereco_id_in INT,
+    	dispositivo_noserie_in INT
 )
 BEGIN
 	IF dispositivo_noserie_in IS NULL 
 		THEN SET dispositivo_noserie_in = create_no_serie(); 
-    END IF;
+    	END IF;
     
 	INSERT INTO tbl_dispositivos VALUES 
-    (NULL, dispositivo_noserie_in, 0);
+    	(NULL, dispositivo_noserie_in, 0);
     
-    SET @NEW_dispositivo_id = get_device_id (
+    	SET @NEW_dispositivo_id = get_dispositivo_id (
 		dispositivo_noserie_in
 	);
     
-    INSERT INTO tbl_cliente_dispositivo VALUES 
-    (cliente_id_in, @NEW_dispositivo_id);
-    
-    INSERT INTO tbl_endereco_dispositivo VALUES
-    (endereco_id_in, @NEW_dispositivo_id);
+    	INSERT INTO tbl_endereco_dispositivo VALUES
+    	(endereco_id_in, @NEW_dispositivo_id);
 END $
 DELIMITER ;
