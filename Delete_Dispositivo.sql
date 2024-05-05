7034aacdaaf017acdaa1151869e7470899a33e15
GabrielRosa835 <150252238+GabrielRosa835@users.noreply.github.com>
2024-05-05 10:32:38 -0300

    Finalizing
    Commented

--- Delete_Dispositivo.sql
+++ Delete_Dispositivo.sql
@@ -1,6 +1,10 @@
 -- CREATE DATABASE db_upxII;
 USE db_upxII;
 
+/* 	Encapsulation for devices deletion.
+	It automatically deletes any relation conected to the device.
+*/
+
 -- DROP PROCEDURE delete_dispositivo;
 DELIMITER $
 CREATE PROCEDURE delete_dispositivo(
@@ -8,6 +12,6 @@ CREATE PROCEDURE delete_dispositivo(
 )
 BEGIN 
 	DELETE FROM tbl_dispositivos WHERE Dispositivo_Id = dispositivo_id_in;
-    	DELETE FROM tbl_endereco_dispositivo WHERE Dispositivo_Id = dispositivo_id_in;
+   	DELETE FROM tbl_endereco_dispositivo WHERE Dispositivo_Id = dispositivo_id_in;
 END $
 DELIMITER ;
